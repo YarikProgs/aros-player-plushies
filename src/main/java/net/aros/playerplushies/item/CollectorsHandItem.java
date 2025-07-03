c997e7575901f2833fd626cd47a86604c560440f
YarikProgs <z.yaroslav2009@gmail.com>
2025-07-03 19:46:44 +0300

    feat: balance changes (1)

--- src/main/java/net/aros/playerplushies/item/CollectorsHandItem.java
+++ src/main/java/net/aros/playerplushies/item/CollectorsHandItem.java
@@ -28,7 +28,7 @@ public class CollectorsHandItem extends Item {
     public static AttributeModifiersComponent createAttributes() {
         return AttributeModifiersComponent
                 .builder()
-                .add(EntityAttributes.GENERIC_ATTACK_DAMAGE, new EntityAttributeModifier(BASE_ATTACK_DAMAGE_MODIFIER_ID, 11, EntityAttributeModifier.Operation.ADD_VALUE), AttributeModifierSlot.MAINHAND)
+                .add(EntityAttributes.GENERIC_ATTACK_DAMAGE, new EntityAttributeModifier(BASE_ATTACK_DAMAGE_MODIFIER_ID, 9, EntityAttributeModifier.Operation.ADD_VALUE), AttributeModifierSlot.MAINHAND)
                 .add(EntityAttributes.GENERIC_ATTACK_SPEED, new EntityAttributeModifier(BASE_ATTACK_SPEED_MODIFIER_ID, -2.4f, EntityAttributeModifier.Operation.ADD_VALUE), AttributeModifierSlot.MAINHAND)
                 .build();
     }
